7a17dfc2ac78fa26b99c4f0af1c2712cf3317f43
Sr-Victor <soloffterminato@gmail.com>
2024-08-18 15:29:38 -0300

    New Update

--- MAIN PATH/Monitoring.py
+++ MAIN PATH/Monitoring.py
@@ -1,20 +1,16 @@
 import os.path
 from datetime import datetime
-import os.path
-from datetime import datetime
-from google.auth.transport.requests import Request  # Certifique-se de que esta linha está correta
+from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 from google_auth_oauthlib.flow import InstalledAppFlow
-import json
-from flask import *
 import pyttsx3 as sx
+
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+CS = ["https://www.googleapis.com/auth/calendar"]
 SAMPLE_SPREADSHEET_ID = "1SjRO_pxgDIq25SZhdIq0wBZb4CQZdvuDLBzJfs-n0Y4"
 RANGES = ["Átrio ' 24 | Escala!A1:C44"]
-
-
 
 def speak(text):
     a = sx.init()
@@ -25,6 +21,7 @@ def get_data():
     creds = None
     if os.path.exists("FEATURES/token.json"):
         creds = Credentials.from_authorized_user_file("FEATURES/token.json", SCOPES)
+    
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -33,9 +30,10 @@ def get_data():
                 "FEATURES/credentials.json", SCOPES
             )
             creds = flow.run_local_server(port=0)
-        with open("FEATURES/token.json", "w") as token:
-            token.write(creds.to_json())
-
+        if creds:
+            with open("FEATURES/token.json", "w") as token:
+                token.write(creds.to_json())
+    
     try:
         service = build("sheets", "v4", credentials=creds)
         sheet = service.spreadsheets()
@@ -51,10 +49,10 @@ def get_data():
 
             for row in values:
                 if len(row) >= 3:
-                    date_str = row[0] if len(row) > 1 else 'Sem escala'  # Assume que a data está na primeira coluna
+                    date_str = row[0] if len(row) > 1 else 'Sem escala'
                     equipe = row[1] if len(row) > 1 else 'Sem equipe'
                     culto = row[2] if len(row) > 2 else 'Sem culto'
-                    print(f"Data lida: {date_str}, Equipe: {equipe}, Culto: {culto}")  # Depuração
+                    print(f"Data lida: {date_str}, Equipe: {equipe}, Culto: {culto}")
                     if "Átrio Music" in equipe:
                         all_data.append({"data": date_str, "equipe": equipe, "culto": culto})
 
@@ -63,28 +61,19 @@ def get_data():
     except HttpError as err:
         print(err)
         return []
-# Envia os dados para uma API Json
-def send_to_api(data):
-    app = Flask(__name__)
-    @app.route('/')
-    def index():
-        return json.dumps({'name': 'alice',
-                        'email': 'alice@outlook.com'})
-    app.run()
-
-
+    
 def find_next_scale(scales):
     today = datetime.now()
     upcoming_scales = []
 
     for scale in scales:
-        scale_date = datetime.strptime(scale['data'], "%d/%m/%Y")  # Ajuste o formato da data se necessário
-        print(f"Comparando com a data de hoje: {scale_date} >= {today}")  # Depuração
+        scale_date = datetime.strptime(scale['data'], "%d/%m/%Y")
+        print(f"Comparando com a data de hoje: {scale_date} >= {today}")
         if scale_date >= today:
             upcoming_scales.append(scale)
 
     if not upcoming_scales:
-        return None  # Nenhuma escala futura encontrada
+        return None
 
     next_scale = min(upcoming_scales, key=lambda x: datetime.strptime(x['data'], "%d/%m/%Y"))
     time_remaining = (datetime.strptime(next_scale['data'], "%d/%m/%Y") - today).days
@@ -92,33 +81,76 @@ def find_next_scale(scales):
     return next_scale, time_remaining
 
 def main():
-    data = get_data()  # Obtém os dados do Google Sheets
-
-    # Encontra a próxima escala e o tempo restante
+    data = get_data()
     next_scale, time_remaining = find_next_scale(data) if data else (None, None)
-
-    # Saída no console
     speak("Exibindo as próximas escalas de Átrio Music")
     print("---------------------------------")
     
-    # Mostra as escalas
     if data:
         for entry in data:
             speak(f"Data: {entry['data']}, Equipe: {entry['equipe']}, Culto: {entry['culto']}")
     else:
         speak("Nenhum dado encontrado.")
-
-    # Adiciona aviso sobre a próxima escala
+    
     if next_scale:
         speak("\nPróxima Escala:")
         speak(f"Data: {next_scale['data']}")
         speak(f"Equipe: {next_scale['equipe']}")
         speak(f"Culto: {next_scale['culto']}")
         speak(f"Faltam {time_remaining} dias para a próxima escala.")
+        
+        creds = None
+        if os.path.exists("FEATURES/token.json"):
+            creds = Credentials.from_authorized_user_file("FEATURES/token.json", CS)
+        
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file("FEATURES/CALENDAR.json", CS)
+                creds = flow.run_local_server(port=0)
+            if creds:
+                with open("FEATURES/token.json", "w") as token:
+                    token.write(creds.to_json())
+        
+        if creds:
+            try:
+                service = build("calendar", "v3", credentials=creds)
+                event = {
+                    'summary': 'Escala | Átrio Music',
+                    'location': 'Assembleia de Deus Quadra 12',
+                    'description': f'{next_scale["culto"]} - Faltam {time_remaining} dias',
+                    'start': {
+                        'dateTime': f'{next_scale["data"]}T18:30:00-03:00',
+                        'timeZone': 'America/Sao_Paulo',
+                    },
+                    'end': {
+                        'dateTime': f'{next_scale["data"]}T21:00:00-03:00',
+                        'timeZone': 'America/Sao_Paulo',
+                    },
+                    'recurrence': [
+                        'RRULE:FREQ=DAILY;COUNT=2'
+                    ],
+                    'attendees': [
+                        {'email': 'contatoeuvictoremmanoel@gmail.com'},
+                        {'email': 'soloffterminato@gmail.com'},
+                    ],
+                    'reminders': {
+                        'useDefault': False,
+                        'overrides': [
+                            {'method': 'email', 'minutes': 24 * 60},
+                            {'method': 'popup', 'minutes': 10},
+                        ],
+                    },
+                }
+                event = service.events().insert(calendarId='primary', body=event).execute()
+                print(f'Event created: {event.get("htmlLink")}')
+            except HttpError as error:
+                print(f"An error occurred: {error}")
+        else:
+            print("Credenciais não foram obtidas corretamente.")
     else:
         speak("\nNenhuma escala futura encontrada.")
-    datas = f"Data: {entry['data']}, Equipe: {entry['equipe']}, Culto: {entry['culto']}"
-    send_to_api(datas)
 
 if __name__ == "__main__":
     main()
--- MAIN PATH/SaraCalendar.py
+++ MAIN PATH/SaraCalendar.py
@@ -1,66 +1,105 @@
+import datetime
 import os.path
+
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-import pyttsx3 as sx
-# Se modificar esses escopos, delete o arquivo token.json.
-SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+from Monitoring import get_data
 
-# ID da planilha e os ranges de exemplo.
-SAMPLE_SPREADSHEET_ID = "1SjRO_pxgDIq25SZhdIq0wBZb4CQZdvuDLBzJfs-n0Y4"
-RANGES = [
-    "Átrio ' 24 | Escala!A1:C44",  # Range da primeira aba
-]
+# If modifying these scopes, delete the file token.json.
+CS = ["https://www.googleapis.com/auth/calendar"]
 
-def speak(text):
-    a = sx.init()
-    a.say(text)
-    a.runAndWait()
-def main():
-    creds = None
-    if os.path.exists("FEATURES/token.json"):
-        creds = Credentials.from_authorized_user_file("FEATURES/token.json", SCOPES)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                "FEATURES/credentials.json", SCOPES
-            )
-            creds = flow.run_local_server(port=0)
-        with open("FEATURES/token.json", "w") as token:
-            token.write(creds.to_json())
 
-    try:
-        service = build("sheets", "v4", credentials=creds)
-        sheet = service.spreadsheets()
 
-        for range_name in RANGES:
-            result = (
-                sheet.values()
-                .get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range=range_name)
-                .execute()
-            )
-            values = result.get("values", [])
+"""Shows basic usage of the Google Calendar API.
+Prints the start and name of the next 10 events on the user's calendar.
+"""
+creds = None
+# The file token.json stores the user's access and refresh tokens, and is
+# created automatically when the authorization flow completes for the first
+# time.
+if os.path.exists("token.json"):
+    creds = Credentials.from_authorized_user_file("token.json", CS)
+# If there are no (valid) credentials available, let the user log in.
+if not creds or not creds.valid:
+    if creds and creds.expired and creds.refresh_token:
+        creds.refresh(Request())
+else:
+    flow = InstalledAppFlow.from_client_secrets_file(
+        "FEATURES/CALENDAR.json", CS
+    )
+    creds = flow.run_local_server(port=0)
+# Save the credentials for the next run
+with open("token.json", "w") as token:
+    token.write(creds.to_json())
 
-            if not values:
-                print(f"No data found in range {range_name}.")
-                continue
+try:
+    service = build("calendar", "v3", credentials=creds)
 
-            print(f"Data from range {range_name}:")
-            for row in values:
-                # Supondo que "EQUIPE" esteja na coluna 2 (índice 1) e "CULTO" na coluna 3 (índice 2)
-                dia = row[0] if len(row) > 1 else 'Sem escala'
-                equipe = row[1] if len(row) > 1 else 'Sem equipe'
-                culto = row[2] if len(row) > 2 else 'Sem culto'
-                if "Átrio Music" in equipe:
-                  print('-----------------------------------------')
-                  print(f"DIA: {dia}, EQUIPE: {equipe}, CULTO: {culto}")
+    # Call the Calendar API
+    now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
+    print("Getting the upcoming 10 events")
+    events_result = (
+        service.events()
+        .list(
+            calendarId="primary",
+            timeMin=now,
+            maxResults=10,
+            singleEvents=True,
+            orderBy="startTime",
+        )
+        .execute()
+    )
+    events = events_result.get("items", [])
 
-    except HttpError as err:
-        print(err)
+
+    if not events:
+        print("No upcoming events found.")
+
+    # Prints the start and name of the next 10 events
+    for event in events:
+        start = event["start"].get("dateTime", event["start"].get("date"))
+        print(start, event["summary"])
+
+    # Refer to the Python quickstart on how to setup the environment:
+    # https://developers.google.com/calendar/quickstart/python
+    # Change the scope to 'https://www.googleapis.com/auth/calendar' and delete any
+    # stored credentials.
+    event = {
+        'summary': 'Google I/O 2024',
+        'location': 'Assembleia de Deus Quadra 12',
+        'description': 'Escala | Átrio Music',
+        'start': {
+            'dateTime': '2024-08-18T09:00:00-03:00',  # Horário de início com fuso horário de São Paulo
+            'timeZone': 'America/Sao_Paulo',
+        },
+        'end': {
+            'dateTime': '2024-08-18T17:00:00-03:00',  # Horário de término com fuso horário de São Paulo
+            'timeZone': 'America/Sao_Paulo',
+        },
+        'recurrence': [
+            'RRULE:FREQ=DAILY;COUNT=2'  # Recorrência diária por 2 dias
+        ],
+        'attendees': [
+            {'email': 'contatoeuvictoremmanoel@gmail.com'},
+            {'email': 'soloffterminato@gmail.com'},
+        ],
+        'reminders': {
+            'useDefault': False,
+            'overrides': [
+                {'method': 'email', 'minutes': 24 * 60},  # Lembrete por e-mail 24 horas antes
+                {'method': 'popup', 'minutes': 10},       # Lembrete pop-up 10 minutos antes
+            ],
+        },
+    }
+
+    event = service.events().insert(calendarId='primary', body=event).execute()
+
+except HttpError as error:
+    print(f"An error occurred: {error}")
+
 
 if __name__ == "__main__":
-    main()
+  main()
