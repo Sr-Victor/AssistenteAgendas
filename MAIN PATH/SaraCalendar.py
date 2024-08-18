221144bef6f9da3760ec399c3f19b2474689a4f2
Sr-Victor <soloffterminato@gmail.com>
2024-08-18 13:11:38 -0300

    Update SaraCalendar.py

--- MAIN PATH/SaraCalendar.py
+++ MAIN PATH/SaraCalendar.py
@@ -18,20 +18,19 @@ def speak(text):
     a = sx.init()
     a.say(text)
     a.runAndWait()
-
 def main():
     creds = None
-    if os.path.exists("token.json"):
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    if os.path.exists("FEATURES/token.json"):
+        creds = Credentials.from_authorized_user_file("FEATURES/token.json", SCOPES)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
             flow = InstalledAppFlow.from_client_secrets_file(
-                "credentials.json", SCOPES
+                "FEATURES/credentials.json", SCOPES
             )
             creds = flow.run_local_server(port=0)
-        with open("token.json", "w") as token:
+        with open("FEATURES/token.json", "w") as token:
             token.write(creds.to_json())
 
     try:
